2a4d7b0eccbef127eea3cbdd4d8d8562f7c35d0b
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-03-22 23:05:15 -0500

    Creating Denari Database

---
+++ src/main/java/com/denarisolutions/denariapp_springboot/DenariAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.denarisolutions.denariapp_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenariAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DenariAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/denarisolutions/denariapp_springboot/DenariAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denarisolutions.denariapp_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenariAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
