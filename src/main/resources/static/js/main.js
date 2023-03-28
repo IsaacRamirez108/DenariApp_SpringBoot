2702929f15f919b4ddcf28362dee98b4fb17cbc8
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-03-28 13:44:35 -0500

    Implementing MapBox Autofill & Confrim Address API together

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,29 +2,42 @@
 (function(){
     // const script = document.getElementById('search-js');
     // script.onload = () => {
-    //     const collection = mapboxsearch.autofill({
-    //         accessToken: MapBox_API_KEY
+    //      mapboxsearch.autofill({accessToken: MapBox_API_KEY});
+    //
+    //     mapboxsearch.config.accessToken = MapBox_API_KEY;
+    //     // Adding confirmation prompt to Users address
+    //     const form = document.querySelector('form');
+    //     form.addEventListener("submit", async (e) => {
+    //         e.preventDefault();
+    //         const result = await confirmAddress(form, {
+    //             minimap: true,
+    //             skipConfirmModal: (feature) =>
+    //                 ['exact', 'high'].includes(
+    //                     feature.properties.match_code.confidence
+    //                 )
+    //         });
+    //         if (result.type === 'nochange') submitForm();
     //     });
     // };
 
 
-    document.getElementById("search-js").onload = () => {
-        mapboxsearch.config.accessToken = MapBox_API_KEY;
+        document.getElementById("search-js").onload = () => {
+            mapboxsearch.config.accessToken = MapBox_API_KEY;
 
-        // Adding confirmation prompt to Users address
-        const form = document.querySelector('form');
-        form.addEventListener("submit", async (e) => {
-            e.preventDefault();
-            const result = await mapboxsearch.confirmAddress(form, {
-                theme: { variables: {border: '3px solid rgba(0,0,0,0.35)', borderRadius: '18px'} },
-                minimap: {
-                    defaultMapStyle: ['mapbox', 'outdoors-v11'],
-                    satelliteToggle: true
-                },
-                skipConfirmModal: (feature) => false // overrides default behavior, show dialog every time
+            // Adding confirmation prompt to Users address
+            const form = document.querySelector('form');
+            form.addEventListener("submit", async (e) => {
+                e.preventDefault();
+                const result = await mapboxsearch.confirmAddress(form, {
+                    theme: { variables: {border: '3px solid rgba(0,0,0,0.35)', borderRadius: '18px'} },
+                    minimap: {
+                        defaultMapStyle: ['mapbox', 'outdoors-v11'],
+                        satelliteToggle: true
+                    },
+                    skipConfirmModal: (feature) => false // overrides default behavior, show dialog every time
+                });
+                console.log(result);
             });
-            console.log(result);
-        });
-    }
+        }
 
 })();
