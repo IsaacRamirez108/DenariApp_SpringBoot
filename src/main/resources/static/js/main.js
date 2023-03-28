b045c46c70b222ea7bb3bed84b7804b2e10cc24d
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-03-28 12:34:01 -0500

    Updates

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,30 @@
 "use strict";
 (function(){
+    // const script = document.getElementById('search-js');
+    // script.onload = () => {
+    //     const collection = mapboxsearch.autofill({
+    //         accessToken: MapBox_API_KEY
+    //     });
+    // };
 
+
+    document.getElementById("search-js").onload = () => {
+        mapboxsearch.config.accessToken = MapBox_API_KEY;
+
+        // Adding confirmation prompt to Users address
+        const form = document.querySelector('form');
+        form.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const result = await mapboxsearch.confirmAddress(form, {
+                theme: { variables: {border: '3px solid rgba(0,0,0,0.35)', borderRadius: '18px'} },
+                minimap: {
+                    defaultMapStyle: ['mapbox', 'outdoors-v11'],
+                    satelliteToggle: true
+                },
+                skipConfirmModal: (feature) => false // overrides default behavior, show dialog every time
+            });
+            console.log(result);
+        });
+    }
 
 })();
