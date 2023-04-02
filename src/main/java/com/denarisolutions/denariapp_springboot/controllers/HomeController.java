cf097ced3c8c2e41749a191278a9988fff52276b
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-04-01 23:57:15 -0500

    Navbar updates

--- src/main/java/com/denarisolutions/denariapp_springboot/controllers/HomeController.java
+++ src/main/java/com/denarisolutions/denariapp_springboot/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/Landing")
     public String homePage() {
         return "index";
     }
