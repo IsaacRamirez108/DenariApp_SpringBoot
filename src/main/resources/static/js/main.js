abc30d7bb02be396d816398d490e4dc4601e3c09
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-04-04 19:33:09 -0500

    Completing SSN text input formatiing

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -120,5 +120,16 @@
         return false;
     })
 
+    // Querying SSN users input to mask all numbers except the last 4 numbers & only allowing a max of 9 numbers total
+    $('.ssn-value').on('keydown keyup mousedown mouseup', function() {
+        let res = this.value, //grabs the value
+            len = res.length, //grabs the length
+            max = 9, //sets a max chars
+            stars = len>0?len>1?len>2?len>3?len>4?'XXX-XX-':'XXX-X':'XXX-':'XX':'X':'', //this provides the masking and formatting
+            result = stars+res.substring(5); //this is the result
+        $(this).attr('maxlength', max); //setting the max length
+        $(".ssn-number").val(result); //spits the value into the input
+    });
+
 
 })();
