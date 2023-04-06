13c49070dcdc94cde4bc969f305c5c6ca2995a2e
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-04-05 19:01:02 -0500

    Adding Addresses table to database

--- src/main/java/com/denarisolutions/denariapp_springboot/models/Address.java
+++ src/main/java/com/denarisolutions/denariapp_springboot/models/Address.java
@@ -1,4 +1,70 @@
 package com.denarisolutions.denariapp_springboot.models;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="addresses")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String street;
+    @Column
+    private String appt_number;
+    @Column
+    private String city;
+    @Column
+    private String state;
+    @Column
+    private String postal_code;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAppt_number() {
+        return appt_number;
+    }
+
+    public void setAppt_number(String appt_number) {
+        this.appt_number = appt_number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
 }
