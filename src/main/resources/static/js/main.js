b0a35d7dbc13bf9a26292aef1bde68b09abc9874
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-04-09 19:00:59 -0500

    Fixing buggs

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -22,24 +22,24 @@
 
 
     // Mapbox Address Confirmation Modal
-    // document.getElementById("search-js").onload = () => {
-    //     mapboxsearch.config.accessToken = MapBox_API_KEY;
-    //
-    //     // Adding confirmation prompt to Users address
-    //     const form = document.querySelector('mapbox-address-autofill');
-    //     form.addEventListener("submit", async (e) => {
-    //         e.preventDefault();
-    //         const result = await mapboxsearch.confirmAddress(form, {
-    //             theme: { variables: {border: '3px solid rgba(0,0,0,0.35)', borderRadius: '18px'} },
-    //             minimap: {
-    //                 defaultMapStyle: ['mapbox', 'outdoors-v11'],
-    //                 satelliteToggle: true
-    //             },
-    //             // skipConfirmModal: (feature) => false // overrides default behavior, show dialog every time
-    //         });
-    //         console.log(result);
-    //     });
-    // }
+    document.getElementById("search-js").onload = () => {
+        mapboxsearch.config.accessToken = MapBox_API_KEY;
+
+        // Adding confirmation prompt to Users address
+        const form = document.querySelector('mapbox-address-autofill');
+        form.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const result = await mapboxsearch.confirmAddress(form, {
+                theme: { variables: {border: '3px solid rgba(0,0,0,0.35)', borderRadius: '18px'} },
+                minimap: {
+                    defaultMapStyle: ['mapbox', 'outdoors-v11'],
+                    satelliteToggle: true
+                },
+                // skipConfirmModal: (feature) => false // overrides default behavior, show dialog every time
+            });
+            console.log(result);
+        });
+    }
 
 
     // Registration Form
