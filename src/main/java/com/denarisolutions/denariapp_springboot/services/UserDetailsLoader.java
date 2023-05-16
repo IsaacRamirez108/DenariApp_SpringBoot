66904fb0de6f2f2eb2fe395da7b43972f336d41a
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-05-16 16:13:46 -0500

    Setting up Spring Security Authentication

---
+++ src/main/java/com/denarisolutions/denariapp_springboot/services/UserDetailsLoader.java
@@ -0,0 +1,28 @@
+package com.denarisolutions.denariapp_springboot.services;
+
+import com.denarisolutions.denariapp_springboot.models.User;
+import com.denarisolutions.denariapp_springboot.models.UserWithRoles;
+import com.denarisolutions.denariapp_springboot.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepository users;
+
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}
